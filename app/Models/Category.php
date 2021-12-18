15bc2dded8dd481c972543c8de17d45ae2e85df4
alaa140101 <alaa140011@gmail.com>
2021-12-18 09:51:39 +0300

    migrations and models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,6 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-=======
-use Illuminate\Database\Eloquent\Factories\HasFactory;
->>>>>>> d44f364f34555b6dac108d96c14e4d7fcfdac7e3
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
