3f12142b3913fbf173136cd229a1f92343b41226
alaa140101 <alaa140011@gmail.com>
2021-12-19 22:16:27 +0300

    13:28 resources

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -2,17 +2,9 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
-=======
-use Illuminate\Database\Eloquent\Factories\HasFactory;
->>>>>>> d44f364f34555b6dac108d96c14e4d7fcfdac7e3
 use Illuminate\Database\Eloquent\Model;
 
 class Rating extends Model
 {
-<<<<<<< HEAD
     //
-=======
-    use HasFactory;
->>>>>>> d44f364f34555b6dac108d96c14e4d7fcfdac7e3
 }
